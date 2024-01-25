0d71c01f64962e1f2ebb78a4b579611b35b9d60e
Pranith Reddy <133804394+pranithk966@users.noreply.github.com>
2024-01-25 09:57:54 +0530

    Add files via upload

---
+++ assessment/index.js
@@ -0,0 +1,109 @@
+const hydd = ['kishore', 'ravi'];
+const bann = ['pandit', 'ganesh'];
+const koll = ['manish', 'raju'];
+const chaa = ['gayatri', 'rashmi'];
+const ahh = ['likith', 'prashanthi'];
+const mumbb = ['harsha', 'lahari'];
+const gurr = ['mahesh', 'balu'];
+const dell = ['aditya', 'divya'];
+const punn = ['divya reddy', 'bhargav'];
+const surr = ['naveen', 'amulya'];
+const chenn = ['bhasker', 'aishwarya'];
+const lucc = ['balu', 'mohan'];
+const jaii = ['shiva', 'rani'];
+
+function hyd(){
+  let ans = '';
+  for(let i=0; i<hydd.length; i++){
+    ans += `<li> ${hydd[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+
+function ban(){
+  let ans = '';
+  for(let i=0; i<bann.length; i++){
+    ans += `<li> ${bann[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+
+function kol(){
+  let ans = '';
+  for(let i=0; i<koll.length; i++){
+    ans += `<li> ${koll[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+function cha(){
+  let ans = '';
+  for(let i=0; i<chaa.length; i++){
+    ans += `<li> ${chaa[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+function ah(){
+  let ans = '';
+  for(let i=0; i<ahh.length; i++){
+    ans += `<li> ${ahh[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+function mumb(){
+  let ans = '';
+  for(let i=0; i<mumbb.length; i++){
+    ans += `<li> ${mumbb[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+
+function gur(){
+  let ans = '';
+  for(let i=0; i<gurr.length; i++){
+    ans += `<li> ${gurr[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+
+function del(){
+  let ans = '';
+  for(let i=0; i<dell.length; i++){
+    ans += `<li> ${dell[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+function pun(){
+  let ans = '';
+  for(let i=0; i<punn.length; i++){
+    ans += `<li> ${punn[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+function sur(){
+  let ans = '';
+  for(let i=0; i<surr.length; i++){
+    ans += `<li> ${surr[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+function chen(){
+  let ans = '';
+  for(let i=0; i<chenn.length; i++){
+    ans += `<li> ${chenn[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+function luc(){
+  let ans = '';
+  for(let i=0; i<lucc.length; i++){
+    ans += `<li> ${lucc[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
+function jai(){
+  let ans = '';
+  for(let i=0; i<jaii.length; i++){
+    ans += `<li> ${jaii[i]} </li>`
+  }
+  document.getElementById('demo').innerHTML = ans;
+}
